9f0a3947177a84b0b221166e140f0d6127749e84
Sergey Sova <i.am@lestad.net>
2017-12-26 15:32:20 +0300

    refactor: Add eslint and refactor to name with atomix

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-'use strict';
+const Q = require('q')
+const conventionalChangelog = require('./conventional-changelog')
+const parserOpts = require('./parser-opts')
+const recommendedBumpOpts = require('./conventional-recommended-bump')
+const writerOpts = require('./writer-opts')
 
-const Q = require(`q`);
-const conventionalChangelog = require(`./conventional-changelog`);
-const parserOpts = require(`./parser-opts`);
-const recommendedBumpOpts = require(`./conventional-recommended-bump`);
-const writerOpts = require(`./writer-opts`);
 
 module.exports = Q.all([conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts])
-  .spread((conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts) => {
-    return {conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts};
-  });
+  // eslint-disable-next-line no-shadow
+  .spread((conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts) => ({
+    conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts,
+  }))
